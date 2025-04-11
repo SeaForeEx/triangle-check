0ed69d8a41686a607ff68cb8bf3e4c4203788626
Charles Bridgers IV (he/him) <113273122+SeaForeEx@users.noreply.github.com>
2025-04-11 10:24:50 -0500

    Update script.js

--- script.js
+++ script.js
@@ -13,80 +13,92 @@ const TriangleInequality = () => {
 
     if (a > 0 && b > 0 && c > 0) {
       if (a + b > c && a + c > b && b + c > a) {
+        let triangleType;
+        
+        // Use a tolerance check to handle floating-point math safely
+        const isRight = Math.abs(c * c - (a * a + b * b)) < 0.0001;
+        
+        if (isRight) {
+          triangleType = "right";
+        } else if (c * c < a * a + b * b) {
+          triangleType = "acute";
+        } else {
+          triangleType = "obtuse";
+        }
         setResult(
-        "This is a triangle! Keep rollin', rollin', rollin', rollin', WHAT?!");
-
+          `This is a ${triangleType} triangle! Keep rollin', rollin', rollin', rollin', WHAT?!`
+        );
       } else {
         setResult(
-        "This is not a triangle! It's just one of those days, when you don't wanna do math!");
-
+          "This is not a triangle! It's just one of those days, when you don't wanna do math!"
+        );
       }
     } else {
       setResult(
-      "Please enter positive numbers for all three sides. Always keep it positive when you ride with Tha Bizkit!");
-
+        "Please enter positive numbers for all three sides. Always keep it positive when you ride with Tha Bizkit!"
+      );
     }
   };
 
-  return /*#__PURE__*/(
-    React.createElement(React.Fragment, null, /*#__PURE__*/
-    React.createElement("div", { className: "title" }, "Fred Durst's Triangle Inequality Theorem"), /*#__PURE__*/
-    React.createElement("div", null, /*#__PURE__*/
-    React.createElement("img", {
-      src: "https://homework.study.com/cimages/multimages/16/tri3159113594440164328.png",
-      alt: "triangle with sides a, b, and c",
-      className: "triangle-img" })), /*#__PURE__*/
-
-
-    React.createElement("div", { className: "side-inputs" }, /*#__PURE__*/
-    React.createElement("div", null, /*#__PURE__*/
-    React.createElement("input", {
-      type: "number",
-      placeholder: "Side a",
-      value: sideA,
-      onChange: e => {
-        setSideA(e.target.value);
-        setResult("");
-      },
-      className: "side-input" })), /*#__PURE__*/
-
-
-    React.createElement("div", null, /*#__PURE__*/
-    React.createElement("input", {
-      type: "number",
-      placeholder: "Side b",
-      value: sideB,
-      onChange: e => {
-        setSideB(e.target.value);
-        setResult("");
-      },
-      className: "side-input" })), /*#__PURE__*/
-
-
-    React.createElement("div", null, /*#__PURE__*/
-    React.createElement("input", {
-      type: "number",
-      placeholder: "Side c",
-      value: sideC,
-      onChange: e => {
-        setSideC(e.target.value);
-        setResult("");
-      },
-      className: "side-input" }))), /*#__PURE__*/
-
-
-
-    React.createElement("button", {
-      onClick: handleTriangle,
-      disabled: !sideA || !sideB || !sideC,
-      className: "submit-button" }, "Check"), /*#__PURE__*/
-
-
-
-    React.createElement("div", { className: "result" }, result)));
-
-
+  return (
+    <>
+      <div className="title">Fred Durst's Triangle Inequality Theorem</div>
+      <div>
+        <img
+          src="https://homework.study.com/cimages/multimages/16/tri3159113594440164328.png"
+          alt="triangle with sides a, b, and c"
+          className="triangle-img"
+        />
+      </div>
+      <div className="side-inputs">
+        <div>
+          <input
+            type="number"
+            placeholder="Side a"
+            value={sideA}
+            onChange={(e) => {
+              setSideA(e.target.value);
+              setResult("");
+            }}
+            className="side-input"
+          />
+        </div>
+        <div>
+          <input
+            type="number"
+            placeholder="Side b"
+            value={sideB}
+            onChange={(e) => {
+              setSideB(e.target.value);
+              setResult("");
+            }}
+            className="side-input"
+          />
+        </div>
+        <div>
+          <input
+            type="number"
+            placeholder="Side c"
+            value={sideC}
+            onChange={(e) => {
+              setSideC(e.target.value);
+              setResult("");
+            }}
+            className="side-input"
+          />
+        </div>
+      </div>
+      <button
+        onClick={handleTriangle}
+        disabled={!sideA || !sideB || !sideC}
+        className="submit-button"
+      >
+        Check
+      </button>
+      <div className="result">{result}</div>
+    </>
+  );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render( /*#__PURE__*/React.createElement(TriangleInequality, null));
+root.render(<TriangleInequality />);
